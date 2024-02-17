972f49322e6b43d55d82506aa4671af1174aa7bd
Scott <scottmanski2011@gmail.com>
2024-02-17 12:25:47 -0500

    initial commit

--- js/data.js
+++ js/data.js
@@ -1,11 +1,30 @@
 
-import QMEMD from '../data/Qualifier Main Event Map Data.json' assert { type: 'json' };
-import MEMD from '../data/Main Event Map Data.json' assert { type: 'json' };
-import QMD from '../data/Qualifier Map Data.json' assert { type: 'json' };
-import MD from '../data/Map Data.json' assert { type: 'json' };
+import AllQ from '../data/All Qualifier Civ Data.json' assert { type: 'json' };
+import AllME from '../data/All Main Event Civ Data.json' assert { type: 'json' };
+import AllQME from '../data/All Qualifier Main Event Civ Data.json' assert { type: 'json' };
+import ArabiaQ from '../data/Arabia Qualifier Civ Data.json' assert { type: 'json' };
+import ArabiaME from '../data/Arabia Main Event Civ Data.json' assert { type: 'json' };
+import ArabiaQME from '../data/Arabia Qualifier Main Event Civ Data.json' assert { type: 'json' };
+import CrossQ from '../data/Cross Qualifier Civ Data.json' assert { type: 'json' };
+import CrossME from '../data/Cross Main Event Civ Data.json' assert { type: 'json' };
+import CrossQME from '../data/Cross Qualifier Main Event Civ Data.json' assert { type: 'json' };
+import GoldRushQ from '../data/Gold Rush Qualifier Civ Data.json' assert { type: 'json' };
+import GoldRushME from '../data/Gold Rush Main Event Civ Data.json' assert { type: 'json' };
+import GoldRushQME from '../data/Gold Rush Qualifier Main Event Civ Data.json' assert { type: 'json' };
+import IslandsQ from '../data/Islands Qualifier Civ Data.json' assert { type: 'json' };
+import IslandsME from '../data/Islands Main Event Civ Data.json' assert { type: 'json' };
+import IslandsQME from '../data/Islands Qualifier Main Event Civ Data.json' assert { type: 'json' };
+import MudflowQ from '../data/Mudflow Qualifier Civ Data.json' assert { type: 'json' };
+import MudflowME from '../data/Mudflow Main Event Civ Data.json' assert { type: 'json' };
+import MudflowQME from '../data/Mudflow Qualifier Main Event Civ Data.json' assert { type: 'json' };
+import QuarryQ from '../data/Quarry Qualifier Civ Data.json' assert { type: 'json' };
+import QuarryME from '../data/Quarry Main Event Civ Data.json' assert { type: 'json' };
+import QuarryQME from '../data/Quarry Qualifier Main Event Civ Data.json' assert { type: 'json' };
+import SlopesQ from '../data/Slopes Qualifier Civ Data.json' assert { type: 'json' };
+import SlopesME from '../data/Slopes Main Event Civ Data.json' assert { type: 'json' };
+import SlopesQME from '../data/Slopes Qualifier Main Event Civ Data.json' assert { type: 'json' };
 
 var dt;
-
 
 function callback(mutationsList) {
   mutationsList.forEach((mutation) => {
@@ -13,6 +32,11 @@ function callback(mutationsList) {
       updateData();
     }
   });
+}
+
+var mapInput = document.getElementById('MCmap');
+mapInput.onchange = (event) => {
+   updateData();
 }
 
 const mutationObserver = new MutationObserver(callback);
@@ -45,9 +69,11 @@ function updateData() {
   if (document.querySelector("#mainevent").classList.contains('active')) {
     file += "ME";
   }
-  file += "MD";
-  console.log(file);
-  dt.rows.add(eval(file));
+  if (file != "") {
+    file = document.getElementById('MCmap').value + file;
+    //console.log(file);
+    dt.rows.add(eval(file));
+  }
   dt.draw();
 }
 
@@ -61,20 +87,21 @@ dt = $('#example').DataTable( {
     pageLength: 100,
     dom: "t",
     columns: [
-      { title: 'Map' },
-      { title: 'Played' },
-      { title: 'First Ban' },
-      { title: 'Ban' },
-      { title: 'Pick' },
-      { title: 'Remaining' },
-      { title: 'First Ban%' },
-      { title: 'Ban%' },
-      { title: 'Pick%' },
-      { title: 'Remaining%' },
+      { title: 'Civilization' },
       { title: 'Wins' },
-      { title: 'Win%' }
+      { title: 'Losses' },
+      { title: 'Win%' },
+      { title: 'First Pick' },
+      { title: 'Pick' },
+      { title: 'Ban' },
+      { title: 'Pass' },
+      { title: 'First Pick%' },
+      { title: 'Pick%' },
+      { title: 'Ban%' },
+      { title: 'Pass%' },
+      { title: 'Played%' }
       ],
-      data: QMD,
+      data: AllQ,
 
     autoWidth: true
 });
